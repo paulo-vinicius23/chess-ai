a714f4c672c27e5d8d8e92be4fc7536d5ce59ba9
unknown <paulo.prado@aluno.uece.br>
2025-11-25 12:29:40 -0300

    Add Tabuleiro

--- chess-frontend/src/app/chessboard/chessboard.ts
+++ chess-frontend/src/app/chessboard/chessboard.ts
@@ -3,20 +3,32 @@ import { Chess } from 'chess.js';
 
 @Component({
   selector: 'app-chessboard',
+  standalone: true,
   templateUrl: './chessboard.html',
-  styleUrl: './chessboard.css',
+  styleUrls: ['./chessboard.css'],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class ChessboardComponent {
   game = new Chess();
 
-  onMove(event: any) {
+  onDrop(e: any) {
+    const { source, target } = e.detail;
+
     const move = this.game.move({
-      from: event.detail.from,
-      to: event.detail.to,
+      from: source,
+      to: target,
       promotion: 'q'
     });
 
-    console.log(move);
+    const board = document.getElementById('board');
+
+    if (move) {
+      // Movimento válido
+      board?.setAttribute('position', this.game.fen());
+    } else {
+      // Movimento inválido → voltar peça para o lugar
+      e.preventDefault(); // ISSO é que bloqueia o movimento
+      board?.setAttribute('position', this.game.fen());
+    }
   }
 }
--- chess-frontend/src/main.ts
+++ chess-frontend/src/main.ts
@@ -1,6 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { App } from './app/app';
 import { provideHttpClient } from '@angular/common/http';
+import 'chessboard-element';
 
 bootstrapApplication(App, {
   providers: [
